03d687885680a73dab198e17487fa68f148dcaac
magura13 <tiagolelis_@hotmail.com>
2022-06-10 15:10:09 -0300

    Start final project mod3

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,17 @@
+import "./style.css";
+import logo from "../../assets/images/logo.svg";
+import bagIcon from "../../assets/icons/bag.svg";
+
+const Header = () => {
+  return (
+    <div className="Header__container">
+      <div>
+        <img className="Logo" src={logo} alt="Logo El Geladon" />
+        <h1>El Geladon</h1>
+      </div>
+      <img className="BagIcon" src={bagIcon} alt="Bag Elgelad Icon" />
+    </div>
+  );
+};
+
+export default Header;
